427aebc9b6ca89952e309b75a19ec67c9fa9139e
S2Bs8W <S2Bs8W@cyber-dojo.org>
2023-02-05 09:48:07 +0000

    34 ran tests, predicted green, got green

--- files/12DaysOfChristmas.js
+++ files/12DaysOfChristmas.js
@@ -1,11 +1,24 @@
 'use strict';
 
-const days = new Array(12).fill();
+const days = [
+  {day: 'first'},
+  {day: 'second'},
+  {day: 'third'},
+  {day: 'fourth'},
+  {day: 'fifth'},
+  {day: 'sixth'},
+  {day: 'seventh'},
+  {day: 'eighth'},
+  {day: 'ninth'},
+  {day: 'tenth'},
+  {day: 'eleventh'},
+  {day: 'twelvth'},
+];
 const secondLine = 'My true love gave to me:';
 const lastLine = 'A partridge in a pear tree.';
 
 function twelveDaysOfChristmasSong() {
-  const stanzas = days.map((day) => `On the first day of Christmas
+  const stanzas = days.map((day) => `On the ${day.day} day of Christmas
 ${secondLine}
 ${lastLine}`);
   return stanzas.join('\n\n');
