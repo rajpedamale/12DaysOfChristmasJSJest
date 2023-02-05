d37e6868ea66213234d46554cd7baffdcb3f0099
S2Bs8W <S2Bs8W@cyber-dojo.org>
2023-02-05 10:18:00 +0000

    50 ran tests, predicted green, got green

--- files/12DaysOfChristmas.js
+++ files/12DaysOfChristmas.js
@@ -15,14 +15,15 @@ const days = [
   {day: 'twelvth'},
 ];
 const secondLine = 'My true love gave to me:';
-const thirdLine = 'Three french hens';
+const thirdLastLine = 'Three french hens';
+const secondLastLine = 'Two turtle doves and'; 
 const lastLine = 'A partridge in a pear tree.';
 
 function twelveDaysOfChristmasSong() {
   const stanzas = days.map((day, index) => `On the ${day.day} day of Christmas
 ${secondLine}
-${index>1 && thirdLine}
-${index && secondLine}
+${index>1 && thirdLastLine}
+${index && secondLastLine}
 ${lastLine}`);
   
   return stanzas.join('\n\n');
