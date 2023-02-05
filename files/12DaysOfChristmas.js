ae55227db46049ecf93bee110324490a46d28f1a
S2Bs8W <S2Bs8W@cyber-dojo.org>
2023-02-05 09:54:57 +0000

    37 ran tests, predicted green, got red

--- files/12DaysOfChristmas.js
+++ files/12DaysOfChristmas.js
@@ -18,8 +18,8 @@ const secondLine = 'My true love gave to me:';
 const lastLine = 'A partridge in a pear tree.';
 
 function twelveDaysOfChristmasSong() {
-  const stanzas = days.map((day) => `On the ${day.day} day of Christmas
-${secondLine}
+  const stanzas = days.map((day, index) => `On the ${day.day} day of Christmas
+${secondLine}${index || 'and'}
 ${lastLine}`);
   return stanzas.join('\n\n');
 }
