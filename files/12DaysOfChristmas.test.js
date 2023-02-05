c511c5914d5370acd071e10e723a0e34a8f9c414
S2Bs8W <S2Bs8W@cyber-dojo.org>
2023-02-05 09:17:02 +0000

    16 ran tests, predicted green, got green

--- files/12DaysOfChristmas.test.js
+++ files/12DaysOfChristmas.test.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const twelveDaysOfChristmasSong = require('./12DaysOfChristmas');
+let poem;
 
 describe('12DaysOfChristmas poem', () => {
+  beforeEach(() => {
+    poem = twelveDaysOfChristmasSong();
+  });
+  
   it('should have 12 stanzas', () => {
-    const poem = twelveDaysOfChristmasSong();
     expect(poem.split('\n\n').length).toEqual(12);
   });
   
   it('should have every stanza end with same line', () => {
-    const stanzas = twelveDaysOfChristmasSong().split('\n\n');
+    const stanzas = poem.split('\n\n');
     stanzas.forEach(stanza => {
       const lastLine = stanza.trim().split('\n').slice(-1)[0];
       expect(lastLine).toEqual('A partridge in a pear tree.');
