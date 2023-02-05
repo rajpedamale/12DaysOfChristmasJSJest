e4fb71a4a5308afce49f8f779df699ea3e785a04
S2Bs8W <S2Bs8W@cyber-dojo.org>
2023-02-05 08:43:42 +0000

    5 ran tests, predicted green, got green

--- files/12DaysOfChristmas.test.js
+++ files/12DaysOfChristmas.test.js
@@ -5,6 +5,6 @@ const TwelveDaysOfChristmasSong = require('./12DaysOfChristmas');
 describe('12DaysOfChristmas poem', () => {
   it('should have 12 stanzas', () => {
     const poem = TwelveDaysOfChristmasSong();
-    expect(poem.split('\n\n')).toEqual(12);
+    expect(poem.split('\n\n').length).toEqual(12);
   });
 });
