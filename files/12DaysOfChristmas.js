c5aa3caaaeebf3bcba2c9185d8c52d26b614ffc8
S2Bs8W <S2Bs8W@cyber-dojo.org>
2023-02-05 10:25:47 +0000

    53 ran tests, predicted red, got red

--- files/12DaysOfChristmas.js
+++ files/12DaysOfChristmas.js
@@ -14,17 +14,30 @@ const days = [
   {day: 'eleventh'},
   {day: 'twelvth'},
 ];
-const secondLine = 'My true love gave to me:';
-const thirdLastLine = 'Three french hens';
-const secondLastLine = 'Two turtle doves and'; 
-const lastLine = 'A partridge in a pear tree.';
+const secondAndLaterLines = [
+  'My true love gave to me:',
+  'Twelve drummers drumming',
+  'Eleven pipers piping',
+  'Ten lords a-leaping',
+  'Nine ladies dancing',
+  'Eight maids a-milking',
+  'Seven swans a-swimming',
+  'Six geese a-laying',
+  'Five golden rings',
+  'Four calling birds',
+  'Three french hens',
+  'Two turtle doves and',
+  'A partridge in a pear tree.'
+];
 
 function twelveDaysOfChristmasSong() {
-  const stanzas = days.map((day, index) => `On the ${day.day} day of Christmas
-${secondLine}
+  const stanzas = days.map((day, index) => {
+    return `On the ${day.day} day of Christmas
+${secondAndLaterLines[0]}
 ${index>1 && thirdLastLine}
 ${index && secondLastLine}
-${lastLine}`);
+${lastLine}`
+});
   
   return stanzas.join('\n\n');
 }
