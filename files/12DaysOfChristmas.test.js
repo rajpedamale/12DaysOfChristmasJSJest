221e3281ad0178d90764d9bef596a4c5de696068
S2Bs8W <S2Bs8W@cyber-dojo.org>
2023-02-05 08:55:36 +0000

    9 ran tests, predicted red, got red

--- files/12DaysOfChristmas.test.js
+++ files/12DaysOfChristmas.test.js
@@ -7,4 +7,11 @@ describe('12DaysOfChristmas poem', () => {
     const poem = twelveDaysOfChristmasSong();
     expect(poem.split('\n\n').length).toEqual(12);
   });
+  
+  it('should have every stanza end with same line', () => {
+    const stanzas = twelveDaysOfChristmasSong().poem.split('\n\n');
+    stanzas.forEach(stanza => {
+        expect(stanza).toEqual('A partridge in a pear tree.');
+    });
+  });
 });
